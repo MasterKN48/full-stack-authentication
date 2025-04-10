610efc884efcccdcb5270fb965fd96640cfbfe80
nitin.kumar <masterkn48@gmail.com>
2025-04-10 19:21:27 +0000

    login api

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('signup')
   async registerUser(@Body() createUserDto: CreateUserDto) {
     return await this.authService.registerUser(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  login(@Request() req: { user: object }) {
+    return req.user;
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,6 +1,11 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './../user/user.service';
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { verify } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +14,15 @@ export class AuthService {
     const user = await this.userService.findByEmail(createUserDto.email);
     if (user) throw new ConflictException('User already exists');
     return this.userService.create(createUserDto);
+  }
+
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) throw new UnauthorizedException('User not found');
+    const isPasswordValid = await verify(user.password, password);
+    if (!isPasswordValid)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return { id: user.id, name: user.name };
   }
 }
